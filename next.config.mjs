0c313a9024e518fa3044debe290ee5f2c2f3c6f2
Evan Schultz <e.schultz.p82@gmail.com>
2025-04-23 07:17:11 -0400

    Migrate images to Vercel Blob

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,21 @@
 const nextConfig = {
   // Add any necessary configuration here
   eslint: {
+    // Comment out or remove this line to enable ESLint during builds
     ignoreDuringBuilds: true,
   },
   typescript: {
+    // Comment out or remove this line to enable TypeScript checks during builds
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com', 'sjc.microlink.io'],
-    unoptimized: true,
+    domains: [
+      '9yxzsogx6jrkcrnz.public.blob.vercel-storage.com', // Your Blob domain
+      'hebbkx1anhila5yf.public.blob.vercel-storage.com', 
+      'public.blob.vercel-storage.com',
+      'sjc.microlink.io'
+    ],
+    unoptimized: false,
   },
   // Explicitly set the output to be standalone to avoid issues with sharp
   output: 'standalone',
